b568a05212a89c903fdfc3dc586388c49b56085e
Muammer Benzes <muammer@muammerbenzes.com>
2026-01-04 17:22:19 +0300

    Uncomment BITS transfer commands for VM downloads

--- Hands-on lab/Resources/PostRebootConfigure.ps1
+++ Hands-on lab/Resources/PostRebootConfigure.ps1
@@ -144,15 +144,15 @@ Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
 # Download SmartHotel VMs from blob storage
 $container = 'https://opsgilitylabs.blob.core.windows.net/public'
 
-#Start-BitsTransfer -Source "$container/SmartHotelWeb1.zip" -Destination "$tempDir\SmartHotelWeb1.zip"
-#Start-BitsTransfer -Source "$container/SmartHotelWeb2.zip" -Destination "$tempDir\SmartHotelWeb2.zip"
-#Start-BitsTransfer -Source "$container/SmartHotelSQL1.zip" -Destination "$tempDir\SmartHotelSQL1.zip"
-#Start-BitsTransfer -Source "$container/UbuntuWAF.zip" -Destination "$tempDir\UbuntuWAF.zip"
+Start-BitsTransfer -Source "$container/SmartHotelWeb1.zip" -Destination "$tempDir\SmartHotelWeb1.zip"
+Start-BitsTransfer -Source "$container/SmartHotelWeb2.zip" -Destination "$tempDir\SmartHotelWeb2.zip"
+Start-BitsTransfer -Source "$container/SmartHotelSQL1.zip" -Destination "$tempDir\SmartHotelSQL1.zip"
+Start-BitsTransfer -Source "$container/UbuntuWAF.zip" -Destination "$tempDir\UbuntuWAF.zip"
 
 # Download the Azure Migrate appliance to save time during the lab
 #$migrateApplianceUrl = Follow-Redirect("https://aka.ms/migrate/appliance/hyperv")
-$migrateApplianceUrl = "https://download.microsoft.com/download/1/d/a/1da26fee-88e3-4c24-bc02-263bc63282d4/AzureMigrateAppliance_v3.20.09.25.zip"
-Start-BitsTransfer -Source $migrateApplianceUrl -Destination "$tempDir\AzureMigrateAppliance.zip"
+#$migrateApplianceUrl = "https://download.microsoft.com/download/1/d/a/1da26fee-88e3-4c24-bc02-263bc63282d4/AzureMigrateAppliance_v3.20.09.25.zip"
+#Start-BitsTransfer -Source $migrateApplianceUrl -Destination "$tempDir\AzureMigrateAppliance.zip"
 
 # Unzip the VMs
 $zipfiles = Get-ChildItem -Path "$tempDir\*.zip"
