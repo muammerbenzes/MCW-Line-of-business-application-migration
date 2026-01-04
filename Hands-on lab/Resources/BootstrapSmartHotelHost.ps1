f990075cc5020539471271cdca49cdfe7454fc2e
Muammer Benzes <muammer@muammerbenzes.com>
2026-01-04 12:58:27 +0300

    Update Chrome installer URL to HTTPS

--- Hands-on lab/Resources/BootstrapSmartHotelHost.ps1
+++ Hands-on lab/Resources/BootstrapSmartHotelHost.ps1
@@ -8,7 +8,7 @@ Stop-Process -Name Explorer
 # Install Chrome
 $Path = $env:TEMP; 
 $Installer = "chrome_installer.exe"
-Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
+Invoke-WebRequest "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
 Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
 Remove-Item $Path\$Installer
 
